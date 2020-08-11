1b0d89b857768c314d41f56c3b95cf387b4e4e0c
e <294097569@qq.com>
2020-08-11 12:01:37 +0800

    添加说明

--- src/main/java/com/ming/community/provider/GithubProvider.java
+++ src/main/java/com/ming/community/provider/GithubProvider.java
@@ -36,6 +36,9 @@ public class GithubProvider {
 
     public GithubUser getUser(String accessToken) {
         OkHttpClient client = new OkHttpClient();
+        //        Request request = new Request.Builder()
+////                .url("https://api.github.com/user?access_token=" + accessToken)
+////                .build();
         Request request = new Request.Builder()
                 .url("https://api.github.com/user")
                 .header("Authorization","token "+accessToken)
